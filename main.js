389676a1099d232073de32e86f0be850083d6314
suryarupHTML <83803411+suryarupHTML@users.noreply.github.com>
2022-10-27 23:06:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("maiodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas');
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log("Modelis Loaded!");
+}
+
+function gotPoses(results) {
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX + "noseY = " + noseY);
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
